4826e7ba964357054f23ce1cafaede25965cb845
sarahellis <sarahellislondon@gmail.com>
2023-07-26 21:09:38 +0100

    Add reminder.py

--- lib/reminder.py
+++ lib/reminder.py
@@ -2,9 +2,13 @@
 class Reminder:
     def __init__(self, name):
         self.name = name
+        self.task = None
 
     def remind_me_to(self, task):
         self.task = task
 
     def remind(self):
+    # Adapted to fail if there is no reminder set, with error message
+        if self.task is None:
+            raise Exception("No reminder set!")
         return f"{self.task}, {self.name}!"
--- tests/test_reminder.py
+++ tests/test_reminder.py
@@ -1,3 +1,4 @@
+import pytest
 from lib.reminder import *
 
 def test_reminder_to_do_a_useful_task():
@@ -5,3 +6,10 @@ def test_reminder_to_do_a_useful_task():
     reminder.remind_me_to("Proofread your paper")
     result = reminder.remind()
     assert result == "Proofread your paper, Albert!"
+
+def test_raises_an_error_when_no_task_is_set():
+    reminder = Reminder("Bettie")
+    with pytest.raises(Exception) as err:
+        reminder.remind()
+    error_message = str(err.value)
+    assert error_message == "No reminder set!"
